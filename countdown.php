0a710bfb5d4dc52a6746dcc1c8650390393a0b54
alexapperley <alexapperley@gmail.com>
2016-07-28 23:23:49 -0500

    Validate

--- countdown.php
+++ countdown.php
@@ -19,7 +19,7 @@
 				<div class="row">
 					<div class="col-md-8 col-md-offset-2">
 						<div class="well">
-							<h2 id=countdown>Script is Probably Still Loading</h2></div>
+							<h2 id=countdown>Script is Probably Still Loading</h2>
 						</div>
 					</div>
 				</div>
